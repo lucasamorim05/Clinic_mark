a427dd5343cb5a4c1dbea3e1e77fc5791262a30a
Lucas de Oliveira Amorim <lcoliveiraamorim50@yahoo.com>
2025-08-14 21:09:31 -0300

    versao 1 - menu adicionado

--- main.c
+++ main.c
@@ -3,6 +3,31 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int opcao;
+
+    do{
+        printf("Bem vindo ao ClinicMark!\n");
+        printf("Digite 1 para cadastrar exame\n");
+        printf("Digite 2 para listar exames\n");
+        printf("Digite 0 para sair\n");
+
+        scanf("%d",&opcao);
+
+        switch(opcao){
+            case 1:
+                cadastrar_exame();
+                break;
+            case 2:
+                listar_exames();
+                break;
+            case 0:
+                printf("Obrigado por usar o ClinicMark")
+                break;
+            default:
+                printf("Digito invalido!\n");
+                break;
+        }
+
+
+    }while (opcao!=0);
 }
